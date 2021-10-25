95c81e2d8b453d0c0602e19858bb5fd0bce5f0bd
eingkim <67992126+eingkim@users.noreply.github.com>
2021-10-26 00:24:36 +0900

    Add files via upload

--- prediction.py
+++ prediction.py
@@ -4,12 +4,12 @@ import serial
 from sklearn.preprocessing import MinMaxScaler
 
 ### ???? ?? ###
-judging_data = 4
+judging_data = 10
 features = 43
 ### ???? ?? ###
-inf = 3000
+inf = 5000
 
-model = models.load_model('CNN2D_model2 (1).h5')
+model = models.load_model('CNN2D_model5.h5')
 
 port = 'COM6'
 baudrate = 9600
@@ -21,32 +21,40 @@ data_list = []
 while True:
 	try:
 		line = ser.readline().decode("utf-8")
-		#line = line[:-1]
-		exec("temp_dic = {" + line + "}")
-		data_list.append(list(temp_dic.values()))
-		while len(data_list) > judging_data:
-			data_list.pop(0)
-		if len(data_list) < judging_data:
-			continue
-		data = np.array(data_list)
-		data_imp = data[:, :-2]
-		data_inf = data[:, -2:-1]
-		data_temp = data[:, -1:]
-		scaler = MinMaxScaler()
-		scaler.fit(data_imp)
-		data_imp = scaler.transform(data_imp)
-		scaler.fit(data_inf)
-		data_inf = scaler.transform(data_inf)
-		scaler.fit(data_temp)
-		data_temp = scaler.transform(data_temp)
-		data = np.concatenate((data_imp, data_inf, data_temp), axis=1)
-		print(data)
-		#data = data.reshape((1, judging_data, features))
-		prediction = model.predict(data.reshape(1, judging_data, features, 1))
-		pred = np.argmax(prediction)
-		print(pred)
-		pred = str(pred).encode('utf-8') 
-		ser.write(pred)
+		if len(line) > 0:
+			print("len(line):{}".format(len(line)))
+			print("line:{}".format(line))
+			exec("temp_arr = np.array([" + line + "])")
+			if len(temp_arr) != features:
+				print(len(temp_arr))
+				continue
+			arr_imp = temp_arr[:-2]
+			arr_inf = temp_arr[-2:-1]
+			arr_tmp = temp_arr[-1:]
+			scaler = MinMaxScaler()
+			scaler.fit(np.array([170.0, inf]).reshape(-1,1))
+			arr_imp = scaler.transform(arr_imp.reshape(-1,1))
+			scaler.fit(np.array([0, 6.0]).reshape(-1, 1))
+			arr_inf = scaler.transform(arr_inf.reshape(-1,1))
+			scaler.fit(np.array([23.2187, 28.6562]).reshape(-1,1))
+			arr_tmp = scaler.transform(arr_tmp.reshape(-1,1))
+			tmp_list = np.concatenate((arr_imp.reshape(1, features-2), arr_inf.reshape(1, 1), arr_tmp.reshape(1,1)), axis=1)
+			data_list.append(tmp_list)
+			print(data_list)
+			while len(data_list) > judging_data:
+				data_list.pop(0)
+			if len(data_list) < judging_data:
+				continue
+			data = np.array(data_list).reshape(judging_data, features)
+			print(data)
+			#data = data.reshape((1, judging_data, features))
+			prediction = model.predict(data.reshape(1, judging_data, features, 1))
+			pred = np.rint(prediction[0])
+			print("prediction:{}".format(prediction))
+			#pred[0] += 1
+			print("pred:{}".format(pred[0]))
+			pred = str(pred[0]).encode('utf-8') 
+			ser.write(pred)
 	except SyntaxError as e0:
 		print(e0)
 		print("Retry")
